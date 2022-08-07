9a1b863284cf5431a2a1ad3806c4810ed09d60ff
renato3x <pereirarenato21@gmail.com>
2022-08-07 13:30:40 -0300

    home: style initialized

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,12 @@
+import addIcon from '../../../public/assets/icons/add.svg'
+import './Home.css'
+
 export default function Home() {
   return (
-    <div></div>
+    <main id="home">
+      <button className="btn-floating purple" id="add-question-btn">
+        <img src={addIcon} alt="add icon" />
+      </button>
+    </main>
   )
 }
