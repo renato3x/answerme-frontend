d111a4b9f496f8c743d0f741b1b9b872248e2f2f
renato3x <pereirarenato21@gmail.com>
2022-08-07 16:53:42 -0300

    application: importing materialize js in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import '../node_modules/modern-css-reset/dist/reset.min.css'
+import 'modern-css-reset/dist/reset.css'
 import './index.css'
-import '../node_modules/materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/css/materialize.min.css'
+import 'materialize-css/dist/js/materialize.min.js'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
