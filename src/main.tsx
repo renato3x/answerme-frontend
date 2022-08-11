98c3b75a1c555229f1d9ec849ad9d059ababdb89
renato3x <pereirarenato21@gmail.com>
2022-08-10 21:32:13 -0300

    material: using other material design css framework

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'modern-css-reset/dist/reset.css'
 import './index.css'
-import 'materialize-css/dist/css/materialize.min.css'
-import 'materialize-css/dist/js/materialize.min.js'
+import 'material-design-lite/dist/material.min'
+import '../node_modules/material-design-lite/dist/material.purple-green.min.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,6 +10,7 @@ export default function Home() {
   useEffect(() => {
     async function getQuestions() {
       const apiQuestions = await AnswerMeApiService.getQuestions()
+      apiQuestions.length = 5
       setQuestions(apiQuestions)
     }
 
@@ -29,13 +30,13 @@ export default function Home() {
                 <p className="answer-quantity">
                   <i className="material-icons">question_answer</i> <span className="quantity">{question.answers?.length}</span>
                 </p>
-                <a href="#" className="btn-flat purple lighten-1 btn-small white-text waves-effect">More</a>
+                <a href="#" className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">More</a>
               </CardActions>
             </Card>
           )
         })}
       </section>
-      <button className="btn-floating purple" id="add-question-btn">
+      <button className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored mdl-button--primary" id="add-question-btn">
         <i className="material-icons">add</i>
       </button>
     </main>
