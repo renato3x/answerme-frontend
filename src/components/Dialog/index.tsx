a2157ed36c841f8c09cb8c535ee8aa330d06c057
renato3x <pereirarenato21@gmail.com>
2022-08-11 16:38:24 -0300

    dialog: interface removed

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -4,9 +4,7 @@ import DialogTitle from './DialogTitle';
 import DialogActions from './DialogActions';
 import DialogTriggerButton from './DialogTriggerButton';
 
-interface DialogProps extends HTMLAttributes<HTMLDialogElement> {}
-
-export const Dialog = forwardRef<HTMLDialogElement, DialogProps>((props, ref) => {
+export const Dialog = forwardRef<HTMLDialogElement, HTMLAttributes<HTMLDialogElement>>((props, ref) => {
   return (
     <dialog {...props} ref={ref} className={`mdl-dialog ${props.className}`}>
       { props.children }
