af86745c43871c1e300651ce14f83cdf81f395a0
renato3x <pereirarenato21@gmail.com>
2022-08-11 22:40:38 -0300

    card component: card content with width 100%

--- src/components/Card/CardContent/index.tsx
+++ src/components/Card/CardContent/index.tsx
@@ -1,8 +1,9 @@
 import React, { HTMLAttributes } from 'react'
+import './CardContent.css'
 
 const CardContent: React.FC<HTMLAttributes<HTMLDivElement>> = ({ children }) => {
   return (
-    <div className="mdl-card__supporting-text">
+    <div className="mdl-card__supporting-text card-content">
       {children}
     </div>
   )
